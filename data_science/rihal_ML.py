4db703a9cde85367e05e6f8bad69c7008a792142
AhmedRaisi <ahmedalraisi@gmail.com>
2023-12-20 09:43:40 +0400

    Basic forestregressor implemented, ValMAE: 759 Test MAE: 3757.

--- data_science/rihal_ML.py
+++ data_science/rihal_ML.py
@@ -1,27 +1,31 @@
 import pandas as pd
-
-# Load the data
-train_df = pd.read_csv('path_to_train_dataset.csv')
-test_df = pd.read_csv('path_to_test_dataset.csv')
-
-# Explore the first few rows
-train_df.head()
-
-# Check for missing values
-train_df.isnull().sum()
-
-# Fill or drop missing values (example)
-train_df.fillna(method='ffill', inplace=True)
-
-# Convert categorical variables to numeric
-train_df = pd.get_dummies(train_df, columns=['manufacturer_name', 'transmission', 'color', ...])
-
-# Example: Create a car's age feature
-train_df['car_age'] = 2023 - train_df['year_produced']
-
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
+
+# Load the data
+train_df = pd.read_csv('data_train.csv')
+test_df = pd.read_csv('data_test.csv')
+
+# Explore the first few rows
+print(train_df.head())
+
+# Check for missing values
+print(train_df.isnull().sum())
+
+# Fill missing values
+train_df.ffill(inplace=True)
+
+# List of categorical columns to convert to dummies
+categorical_columns = ['manufacturer_name', 'transmission', 'color', 
+                       'engine_fuel', 'engine_type', 'body_type', 
+                       'ownership', 'type_of_drive']
+
+# Convert categorical variables to numeric
+train_df = pd.get_dummies(train_df, columns=categorical_columns)
+
+# Example: Create a car's age feature
+train_df['car_age'] = 2023 - train_df['year_produced']
 
 # Splitting the training set for validation
 X = train_df.drop('price_usd', axis=1)
@@ -42,12 +46,23 @@ val_mae = mean_absolute_error(y_val, val_predictions)
 print(f"Validation MAE: {val_mae}")
 
 # Preprocess the test set similarly to the training set
-test_df_processed = pd.get_dummies(test_df, columns=['manufacturer_name', 'transmission', 'color', ...])
-# Note: Ensure the test set has the same feature columns as the training set
+# Fill missing values in the test set
+test_df.ffill(inplace=True)
+
+# Convert categorical variables to numeric for the test set
+test_df_processed = pd.get_dummies(test_df, columns=categorical_columns)
+
+# Align the columns of test_df with train_df, adding missing columns filled with 0
+test_df_processed = test_df_processed.reindex(columns=train_df.columns, fill_value=0)
+
+# Drop the 'price_usd' column from test_df_processed if it's not needed for predictions
+test_df_processed = test_df_processed.drop(columns=['price_usd'], errors='ignore')
 
 # Make final predictions on the test set
-test_predictions = model.predict(test_df_processed.drop('price_usd', axis=1))
+test_predictions = model.predict(test_df_processed)
 
 # Calculate MAE on the test set
-test_mae = mean_absolute_error(test_df_processed['price_usd'], test_predictions)
+# Assume 'original_test_df' is your original test dataset with the actual prices
+# If the original test dataset contains the 'price_usd' column, use it for MAE calculation
+test_mae = mean_absolute_error(test_df['price_usd'], test_predictions)
 print(f"Test MAE: {test_mae}")
